8b45119cb31e1e097e37a291e230458b93b49025
Guru901 <133991448+Guru901@users.noreply.github.com>
2023-05-18 15:40:07 +0000

    Add files via upload

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{ 
+    public float speed = 10f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical) * speed;
+        rb.velocity = movement;
+    }
+}
+
